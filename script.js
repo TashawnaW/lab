0084819c6120cc209438ddfb05b687f76fd98324
TashawnaW <tashawnawells@gmail.com>
2024-09-12 00:27:34 -0400

    added part two

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ for (let i = 1; i <= 100; i++) {
     if (i % 5 === 0) output += 'Buzz';
     console.log(output || i);
 }
+
+// Part 2: Prime Numbers 
+
+function findNextPrime(n) {
+    function isPrime(num) {
+        if (num <= 1) return false;
+        for (let i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i === 0) return false;
+        }
+        return true;
+    }
+
+    let nextNum = Math.floor(n) + 1;
+    while (!isPrime(nextNum)) {
+        nextNum++;
+    }
+    return nextNum;
+}
